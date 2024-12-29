086434142ec65427140154b86d37cdd1e545b52e
HenriqueCCdA <HenriqueCCdA>
2024-12-29 16:00:59 +0000

    m

--- dia02/Resolucao_exercicios.sql
+++ dia02/Resolucao_exercicios.sql
@@ -125,4 +125,20 @@ WHERE descTipoPagamento = 'credit_card' AND nrParcelas >=2 AND vlPagamento / 2 <
 
 -- COMMAND ----------
 
+-- Selecione um dia de venda aleatório de cada vendedor
 
+WITH tb_dia_vendedor AS (
+  SELECT t1.idVendedor,
+    date(t2.dtPedido) AS dtPedido
+
+  FROM silver.olist.item_pedido AS t1
+
+  INNER JOIN silver.olist.pedido AS t2
+  ON t1.idPedido = t2.idPedido
+) 
+
+SELECT *, 
+  row_number() OVER (PARTITION BY idVendedor ORDER BY RAND()) AS rnVendedor
+FROM tb_dia_vendedor
+
+QUALIFY rnVendedor = 1
